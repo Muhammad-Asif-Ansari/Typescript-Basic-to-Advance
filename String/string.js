d97dc5ac741d17b885755ed6cd521870c43d1684
Muhammad-Asif-Ansari <muhammadasifansari101@gmail.com>
2025-08-26 23:00:12 +0500

    more ways of uses string

--- String/string.js
+++ String/string.js
@@ -1,1 +1,5 @@
 var myName = "Muhammad Asif Ansari";
+var hello = "how are you?";
+var age = 20;
+var userName = "Ahmed";
+var infoUser = "It's your name is ".concat(userName, " and\nage is ").concat(age);
--- String/string.ts
+++ String/string.ts
@@ -1,1 +1,10 @@
 var myName:string = "Muhammad Asif Ansari";
+
+var hello:string =`how are you?`
+
+var age:number = 20;
+
+var userName:string = "Ahmed";
+
+var infoUser:string = `It's your name is ${userName} and
+age is ${age}`
