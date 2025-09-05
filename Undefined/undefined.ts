b62af64f25535e98ba6460eaeb591f0b23ff0d4b
Muhammad Asif Ansari <muhammadasifansari101@gmail.com>
2025-09-05 23:42:15 +0500

    Update undefined.ts

--- Undefined/undefined.ts
+++ Undefined/undefined.ts
@@ -6,3 +6,29 @@ var val = null;
 userName1 = "Awais";
 console.log(val == userName1);
 
+// TypeScript Example
+
+// Interface define karte hain
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  isAdmin?: boolean; // optional property
+}
+
+// Function jo user create karega
+function createUser(user: User): string {
+  return `User ${user.name} with email ${user.email} created successfully!`;
+}
+
+// Ek user object
+const newUser: User = {
+  id: 1,
+  name: "Muhammad Asif Ansari",
+  email: "muhammadasifansari101@gmail.com",
+  isAdmin: true
+};
+
+// Function call
+console.log(createUser(newUser));
+
